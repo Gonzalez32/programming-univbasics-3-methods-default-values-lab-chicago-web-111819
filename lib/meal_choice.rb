2a78877bd5f52ad4a8f308639bb280746e2dd827
Juan Gonzalez <3232rose@gmail.com>
2019-10-22 01:34:16 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,11 @@
-# Your code here
+
+meal_choice("veg1" "veg2" "protein")
+puts "What a nutritious meal!"
+puts "A plate of #{protein} with #{veg1} and #{veg2}."
+end
+return "A plate of #{protein} with #{veg1} and #{veg2}."
+
+
+# An error occurred while loading ./spec/meal_choice_spec.rb.
+Failure/Error: CREATED_FILES.each{ |f| require f }
+# some-how I pass the test! 
