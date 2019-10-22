8385540b0be82cdb0897f4dd655e5647d541f362
Juan Gonzalez <3232rose@gmail.com>
2019-10-22 01:33:26 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@ meal_choice("veg1" "veg2" "protein")
 puts "What a nutritious meal!"
 puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
-return 
+return "A plate of #{protein} with #{veg1} and #{veg2}."
 
 # some-how I pass the test! 
 # An error occurred while loading ./spec/meal_choice_spec.rb.
